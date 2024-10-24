2cfe47054701db135593fd91b6d073cbf49f9715
Armsnake88 <gminor0e@proton.me>
2024-10-24 23:02:35 +0000

    Add files via upload

--- ChessEngine.py
+++ ChessEngine.py
@@ -1,5 +1,3 @@
-
-
 class GameState:
     def __init__(self):
         '''
@@ -26,6 +24,7 @@ class GameState:
             self.white_to_move = not self.white_to_move
 
     def getValidMoves(self):
+        # This should include logic to check for checks
         return self.getAllPossibleMoves()  # ignoring checks for now
 
     def getAllPossibleMoves(self):
@@ -35,11 +34,18 @@ class GameState:
                 turn = self.board[r][c][0]
                 if (turn == 'w' and self.white_to_move) or (turn == 'b' and not self.white_to_move):
                     piece = self.board[r][c][1]
-
-                    if piece == 'p':
+                    if piece == 'P':  # Pawn
                         self.getPawnMoves(r, c, moves)
-                    elif piece == 'R':
+                    elif piece == 'N':  # Knight
+                        self.getKnightMoves(r, c, moves)
+                    elif piece == 'B':  # Bishop
+                        self.getBishopMoves(r, c, moves)
+                    elif piece == 'R':  # Rook
                         self.getRookMoves(r, c, moves)
+                    elif piece == 'Q':  # Queen
+                        self.getQueenMoves(r, c, moves)
+                    elif piece == 'K':  # King
+                        self.getKingMoves(r, c, moves)
         return moves
 
     def getPawnMoves(self, r, c, moves):
@@ -48,19 +54,11 @@ class GameState:
                 moves.append(Move((r, c), (r - 1, c), self.board))
                 if r == 6 and self.board[r - 2][c] == "--":  # Double move from starting position
                     moves.append(Move((r, c), (r - 2, c), self.board))
-            '''  
-            Remember: r and c represent the current index for a piece.
-            For white pawns, moving "forward" means moving to a lower row number  
-            they start from higher row numbers and move towards row 0.   
-            ^ For black pawns, it means moving to a higher row number
-            r > 0 and c > 0 keeps the pieces within the board when moving 
-            '''
             # Captures
             if c > 0 and self.board[r - 1][c - 1][0] == 'b':  # Capture left
                 moves.append(Move((r, c), (r - 1, c - 1), self.board))
             if c < len(self.board[r]) - 1 and self.board[r - 1][c + 1][0] == 'b':  # Capture right
                 moves.append(Move((r, c), (r - 1, c + 1), self.board))
-
             # En passant
             # Add logic for en passant here
 
@@ -69,27 +67,84 @@ class GameState:
                 moves.append(Move((r, c), (r + 1, c), self.board))
                 if r == 1 and self.board[r + 2][c] == "--":  # Double move from starting position
                     moves.append(Move((r, c), (r + 2, c), self.board))
-
             # Captures
             if c > 0 and self.board[r + 1][c - 1][0] == 'w':  # Capture left
                 moves.append(Move((r, c), (r + 1, c - 1), self.board))
             if c < len(self.board[r]) - 1 and self.board[r + 1][c + 1][0] == 'w':  # Capture right
                 moves.append(Move((r, c), (r + 1, c + 1), self.board))
-
             # En passant
             # Add logic for en passant here
 
     def getRookMoves(self, r, c, moves):
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # Up, Down, Left, Right
+        enemy_color = 'b' if self.white_to_move else 'w'
+        for d in directions:
+            for i in range(1, 8):
+                end_row = r + d[0] * i
+                end_col = c + d[1] * i
+                if 0 <= end_row < 8 and 0 <= end_col < 8:  # Check if on board
+                    end_piece = self.board[end_row][end_col]
+                    if end_piece == "--":  # Empty square
+                        moves.append(Move((r, c), (end_row, end_col), self.board))
+                    elif end_piece[0] == enemy_color:  # Enemy piece
+                        moves.append(Move((r, c), (end_row, end_col), self.board))
+                        break
+                    else:  # Friendly piece
+                        break
+                else:
+                    break
 
-        pass
     def getQueenMoves(self, r, c, moves):
-        pass
+        self.getRookMoves(r, c, moves)  # Queen moves like a rook
+        self.getBishopMoves(r, c, moves)  # Queen moves like a bishop
+
     def getKnightMoves(self, r, c, moves):
-        pass
+        knight_moves = [
+            (-2, -1), (-1, -2), (1, -2), (2, -1),
+            (2, 1), (1, 2), (-1, 2), (-2, 1)
+        ]
+        ally_color = 'w' if self.white_to_move else 'b'
+        for m in knight_moves:
+            end_row = r + m[0]
+            end_col = c + m[1]
+            if 0 <= end_row < 8 and 0 <= end_col < 8:
+                end_piece = self.board[end_row][end_col]
+                if end_piece[0] != ally_color:
+                    moves.append(Move((r, c), (end_row, end_col), self.board))
+
     def getKingMoves(self, r, c, moves):
-        pass
+        king_moves = [
+            (-1, -1), (-1, 0), (-1, 1),
+            (0, -1), (0, 1),
+            (1, -1), (1, 0), (1, 1)
+        ]
+        ally_color = 'w' if self.white_to_move else 'b'
+        for i in range(8):
+            end_row = r + king_moves[i][0]
+            end_col = c + king_moves[i][1]
+            if 0 <= end_row < 8 and 0 <= end_col < 8:
+                end_piece = self.board[end_row][end_col]
+                if end_piece[0] != ally_color:
+                    moves.append(Move((r, c), (end_row, end_col), self.board))
+
     def getBishopMoves(self, r, c, moves):
-        pass
+        directions = [(-1, -1), (-1, 1), (1, -1), (1, 1)]  # Diagonal directions
+        enemy_color = 'b' if self.white_to_move else 'w'
+        for d in directions:
+            for i in range(1, 8):
+                end_row = r + d[0] * i
+                end_col = c + d[1] * i
+                if 0 <= end_row < 8 and 0 <= end_col < 8:
+                    end_piece = self.board[end_row][end_col]
+                    if end_piece == "--":
+                        moves.append(Move((r, c), (end_row, end_col), self.board))
+                    elif end_piece[0] == enemy_color:
+                        moves.append(Move((r, c), (end_row, end_col), self.board))
+                        break
+                    else:
+                        break
+                else:
+                    break
 
     def make_move(self, move):
         # Generate all valid moves
@@ -102,7 +157,10 @@ class GameState:
             self.board[move.end_row][move.end_col] = move.piece_moved
             self.white_to_move = not self.white_to_move
             self.move_log.append(move)
-
+            # Handle special moves (e.g., promotion, castling)
+            # Example for pawn promotion:
+            if move.piece_moved[1] == 'P' and (move.end_row == 0 or move.end_row == 7):
+                self.board[move.end_row][move.end_col] = move.piece_moved[0] + 'Q'  # Promote to queen
 
 
 class Move:
@@ -114,7 +172,6 @@ class Move:
         self.piece_moved = board[self.start_row][self.start_col]
         self.piece_captured = board[self.end_row][self.end_col]
         self.moveID = self.start_row * 1000 + self.start_col * 100 + self.end_row * 10 + self.end_col
-
 
     def get_chess_notation(self):
         files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
